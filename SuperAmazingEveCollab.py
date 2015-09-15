b921a7c632d4a898ce545818356142a42b32e9fd
Steven <stevensmiley@gmail.com>
2015-09-14 18:30:34 -0700

    Fixing truths

--- SuperAmazingEveCollab.py
+++ SuperAmazingEveCollab.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hammy is definitely the coolest person I know.'
+    return 'Hammy is definitely the coolest person I know. But Steven will always be smarter.'
 
 
 if __name__ == '__main__':
