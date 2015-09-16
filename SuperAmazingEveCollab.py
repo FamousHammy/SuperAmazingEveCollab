d56093b54dcd7ee9211878127310aadc912e9424
Steven <stevensmiley@gmail.com>
2015-09-15 21:33:55 -0700

    Setting item structure, sorting, and page loop to display items in the table

--- SuperAmazingEveCollab.py
+++ SuperAmazingEveCollab.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from operator import itemgetter
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def top_150():
-    return render_template('top-150.html')
+    '''this is just a placeholder for now, but demonstrates the structure we need'''
+    items = [['Weak Pea Shooter Thing', 3],
+             ['Super Ridiculous Ion Cannon', 7]]
+    sorted_items = sorted(items, key=itemgetter(1), reverse=True)
+    return render_template('top-150.html', items=sorted_items)
 
 
 if __name__ == '__main__':
